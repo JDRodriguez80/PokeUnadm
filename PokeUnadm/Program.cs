7015718bb1366ad7a0a0e0dfbbc0ee7c05858897
JESUS DARIO RODRIGUEZ <jesusrdz80@nube.unadmexico.mx>
2022-04-12 13:20:33 -0500

    agregando menu

--- PokeUnadm/Program.cs
+++ PokeUnadm/Program.cs
@@ -25,12 +25,60 @@ $$    $$/ $$ |  $$ |$$    $$/ $$    $$ |      $$    $$/ $$    $$/ $$ |       $$ 
             Console.WriteLine(arte);
             System.Threading.Thread.Sleep(5000);//
             Console.Clear();
-            
-
-
-
+            menu();
         }
+        static void menu() {
+            int aux = 1;
+            while (aux != 0) {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("1.- Crear Pokemon ");
+                Console.WriteLine("");
+                Console.WriteLine("2.- Alimentar Pokemon");
+                Console.WriteLine("");
+                Console.WriteLine("3.- Estadisticas");
+                Console.WriteLine("");
+                Console.WriteLine("4.- Ataque Básico");
+                Console.WriteLine("");
+                Console.WriteLine("5.- Ataque Especial");
+                Console.WriteLine("");
+                Console.WriteLine("6.- Salir");
+                Console.WriteLine("");
+                Console.WriteLine("=======================================");
+                Console.WriteLine("Seleccione una opcion ...");
+                Console.WriteLine("");
+                int seleccion = (int)Convert.ToInt32(Console.ReadLine());
 
-
+                //Switch para controlar opciones
+                switch (seleccion)
+                {
+                    case 1:
+                        crearPokemon();
+                        break;
+                    case 2:
+                        alimentarPokemon();
+                        break;
+                    case 3:
+                        estadisticas();
+                        break;
+                    case 4:
+                        ataqueBasico();
+                        break;
+                    case 5:
+                        ataqueEspecial();
+                        break;
+                    case 6:
+                        aux = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Eliga una opción valida");
+                        break;      
+                }
+            }
+        }
+        static void crearPokemon() { }
+        static void alimentarPokemon() { }
+        static void estadisticas() { }
+        static void ataqueBasico() { }
+        static void ataqueEspecial() { }
     }
 }
